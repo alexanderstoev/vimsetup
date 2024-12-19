a0de7fe550b977f111a4044ce651467ad032c6a9
Alex Stoev <alex.stoev@gmail.com>
2024-12-20 01:42:51 +0200

    Initial commit with file exporer, color scheme and which-key

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,14 @@
+return {
+	"catppuccin/nvim", 
+	name = "catppuccin", 
+	priority = 1000, 
+	lazy=false,
+	config = {
+		flavor="latte",
+		transparent_background=true,
+		no_italic=false,
+		styles={
+			comments={"italic"}
+		}
+	}
+}
